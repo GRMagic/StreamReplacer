1e751c33f05b2be7666350f771e9b44c32ad8f53
GRMagic <gustavo.rm90@gmail.com>
2020-06-03 00:03:25 -0300

    first commit

---
+++ StreamReplaced.Test/UnitTest1.cs
@@ -0,0 +1,94 @@
+using System;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace StreamReplaced.Test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("Traga-me um café!", "um café", "uma cerveja", "Traga-me uma cerveja!")]
+        [InlineData("Qual é seu nome?", "nome", "email", "Qual é seu email?")]
+        [InlineData("Trocar o começo", "Trocar", "Mudar", "Mudar o começo")]
+        [InlineData("Trocar o fim", "fim", "final", "Trocar o final")]
+        [InlineData("Eu gosto de coca", "coca", "coco", "Eu gosto de coco")]
+        [InlineData("Trocar as letras a da frase", "a", "X", "TrocXr Xs letrXs X dX frXse")]
+        [InlineData("Não encontrar", "abc", "XXX", "Não encontrar")]
+        [InlineData("AABBCC", "B", "X", "AAXXCC")]
+        [InlineData("AABBCC", "B", "XXX", "AAXXXXXXCC")]
+        [InlineData("AABBCC", "B", "", "AACC")]
+        [InlineData("ABC", "C", "+", "AB+")]
+        [InlineData("ABC", "A", "X", "ABC", 1)]
+        [InlineData("ABCA", "A", "X", "ABCX", 1)]
+        [InlineData("ABCA", "A", "X", "XBCA", 0, 2)]
+        [InlineData("ABCA", "A", "X", "ABCA", 1, 2)]
+        [InlineData("ABCA", "A", "X", "ABCX", 1, 2000)]
+        public void StreamReplaced_ByteReplace_SubstituirComSucesso(string content, string from, string to, string expected, int start = 0, int? end = null)
+        {
+            // Arrange
+            var bContent = Encoding.UTF8.GetBytes(content);
+            var bFrom = Encoding.UTF8.GetBytes(from);
+            var bTo = Encoding.UTF8.GetBytes(to);
+
+            // Act
+            var result = StreamReplaced.ByteReplace(bContent, bFrom, bTo, start, end ?? bContent.Length);
+
+            //Assert
+            var sResult = Encoding.UTF8.GetString(result);
+            Assert.Equal(expected, sResult );
+
+        }
+
+        [Theory]
+        [InlineData("Traga-me um café!", "café", "chá", "Traga-me um chá!")]
+        [InlineData("Traga-me um café!", "um café", "uma cerveja", "Traga-me uma cerveja!")]
+        [InlineData("Qual é seu nome?", "nome", "email", "Qual é seu email?")]
+        [InlineData("Trocar o começo", "Trocar", "Mudar", "Mudar o começo")]
+        [InlineData("Trocar o fim", "fim", "final", "Trocar o final")]
+        [InlineData("Eu gosto de coca", "coca", "coco", "Eu gosto de coco")]
+        [InlineData("Trocar as letras a da frase", "a", "X", "TrocXr Xs letrXs X dX frXse")]
+        [InlineData("Não encontrar", "abc", "XXX", "Não encontrar")]
+        [InlineData("AABBCC", "B", "X", "AAXXCC")]
+        [InlineData("AABBCC", "B", "XXX", "AAXXXXXXCC")]
+        [InlineData("AABBCC", "B", "", "AACC")]
+        [InlineData("ABC", "C", "+", "AB+")]
+        [InlineData("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            "Lorem", "???",
+            "??? Ipsum is simply dummy text of the printing and typesetting industry. ??? Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing ??? Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of ??? Ipsum.")]
+        [InlineData("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+            ".", ";",
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry; Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book; It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged; It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum;")]
+        public void StreamReplaced_Read_SubstituirComSucesso(string content, string from, string to, string expected)
+        {
+            // Arrange
+            var stream = GenerateStreamFromString(content);
+            var sReplaced = new StreamReplaced(stream);
+
+            var bFrom = Encoding.UTF8.GetBytes(from);
+            var bTo = Encoding.UTF8.GetBytes(to);
+
+            sReplaced.Replace(bFrom, bTo);
+
+            var sr = new StreamReader(sReplaced, Encoding.UTF8, false, 128);
+
+            // Act
+            var result = sr.ReadToEnd();
+            sr.Dispose();
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+    }
+}
---
+++ StreamReplaced/StreamReplaced.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StreamReplaced
+{
+    public class StreamReplaced : Stream
+    {
+        private Stream BaseStream;
+        
+        private List<byte> Buffer = null;
+        private int BufLen = 0;
+
+        private Dictionary<byte[], byte[]> KeyValuePairs = new Dictionary<byte[], byte[]>();
+
+        public StreamReplaced(Stream baseStream)
+        {
+            this.BaseStream = baseStream ?? throw new ArgumentNullException("baseStream", "Null streams are not allowed.");
+        }
+
+        public void Replace(byte[] from, byte[] to)
+        {
+            if (Buffer != null) throw new Exception("You cannot define a replacement after reading begins.");
+
+            KeyValuePairs.Add(from, to);
+            var l = from.Length;
+            if (l > BufLen) BufLen = l;
+        }
+
+        public override bool CanRead => true;
+
+        public override bool CanSeek => false;
+
+        public override bool CanWrite => false;
+
+        public override long Length => throw new NotImplementedException();
+
+        public override long Position { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void Flush()
+        {
+            BaseStream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (Buffer == null) Buffer = new List<byte>(BufLen);
+            int len = Math.Max(0, count + BufLen - Buffer.Count);
+            byte[] temp = new byte[len];
+            len = BaseStream.Read(temp, 0, temp.Length);
+            for(int i = 0; i< len; i++) Buffer.Add(temp[i]);
+            temp = Buffer.ToArray();
+
+            foreach (var kv in KeyValuePairs) {
+                temp = ByteReplace(temp, kv.Key, kv.Value, 0, temp.Length);
+            }
+            Buffer = new List<byte>(temp);
+
+            int returnLen = Math.Min(temp.Length, count);
+            for(int i=0; i< returnLen; i++) buffer[i+offset] = temp[i];
+            Buffer.RemoveRange(0, returnLen);
+
+            return returnLen; 
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            BaseStream.Dispose();
+        }
+
+        public static byte[] ByteReplace(byte[] content, byte[] from, byte[] to, int start, int end)
+        {
+            if (from.Length == 0) return content;
+
+            var result = new List<byte>();
+
+            for(int i = 0; i < content.Length; i++)
+            {
+                if(i < start || i >= end)
+                {
+                    result.Add(content[i]);
+                    continue;
+                }
+                
+                bool match = true;
+                for (int j = 0; j < from.Length; j++)
+                {
+                    if (i + j > end || content[i + j] != from[j])
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match)
+                {
+                    result.AddRange(to);
+                    i += from.Length - 1;
+                }
+                else
+                {
+                    result.Add(content[i]);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
